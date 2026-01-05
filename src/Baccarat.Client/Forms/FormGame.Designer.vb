cb31f927e8da94e6347301b863e90ed924e40cae
YH <ykthri11201@gamil.com>
2026-01-06 02:18:23 +0900

    Add VS solution and SDK-style VB projects

---
+++ src/Baccarat.Client/Forms/FormGame.Designer.vb
@@ -0,0 +1,15 @@
+Imports System.Windows.Forms
+
+Namespace Baccarat.Client.Forms
+    Partial Class FormGame
+        Inherits Form
+
+        Private Sub InitializeComponent()
+            Me.SuspendLayout()
+            Me.ClientSize = New Drawing.Size(800, 600)
+            Me.Name = "FormGame"
+            Me.Text = "Baccarat - Game"
+            Me.ResumeLayout(False)
+        End Sub
+    End Class
+End Namespace
